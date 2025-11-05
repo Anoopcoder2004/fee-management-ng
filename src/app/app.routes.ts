4424d273bc45ef4410b3a16681383cbeb3d7c932
Anoop <Anooppopziyan@gmail.com>
2025-11-05 15:29:47 +0530

    added member page, (with zindex issue)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
-import { LayoutComponent } from './layout/layout/layout.component';
+import { MemberComponent } from './modules/member/member.component';
+import { AppComponent } from './app.component';
 
 export const routes: Routes = [
-    { path: '', component: LayoutComponent },
+    { path: '', component: AppComponent },
+    { path: 'members', component: MemberComponent }
 
 ];

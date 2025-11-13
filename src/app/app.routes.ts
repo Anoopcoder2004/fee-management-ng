da8da66a0288870dd99798f49cb50adff6d2b113
Anoop <Anooppopziyan@gmail.com>
2025-11-13 12:30:09 +0530

    added members component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,14 +2,16 @@ import { Routes } from '@angular/router';
 import { LayoutComponent } from './layout/layout/layout.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { LoginComponent } from './modules/login/login.component';
-
+import { MemberComponent } from './modules/member/member.component';
 export const routes: Routes = [
   {
     path: '',
     component: LayoutComponent,
     children: [
       { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
-      { path: 'dashboard', component: DashboardComponent }
+      { path: 'dashboard', component: DashboardComponent },
+      { path: 'member',component: MemberComponent },
+
     ]
   },
   { path: 'login', component: LoginComponent } // future layout-free route

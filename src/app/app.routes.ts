35f2b0b8589344cc77948d9a83ce27fd8f4f09ef
Anoop <Anooppopziyan@gmail.com>
2025-11-28 15:46:09 +0530

    bootstrap installed

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,9 @@
 import { Routes } from '@angular/router';
 import { LayoutComponent } from './layout/layout/layout.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
 import { LoginComponent } from './modules/login/login.component';
 import { MemberComponent } from './modules/member/member.component';
 import { ComingSoonComponent } from './coming-soon/coming-soon.component';
+import { DashboardComponent } from './modules/dashboard/dashboard.component';
 
 export const routes: Routes = [
   {

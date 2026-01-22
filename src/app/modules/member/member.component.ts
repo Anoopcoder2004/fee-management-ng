9a33ede09deaae4caef1463dd84407de6369a220
Anoop <Anooppopziyan@gmail.com>
2026-01-22 20:52:03 +0530

    client api integrated

--- src/app/modules/member/member.component.ts
+++ src/app/modules/member/member.component.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { ClientService } from '../../shared/service/client.service';
 
 interface Member {
   id: number;
@@ -18,21 +19,39 @@ interface Member {
   styleUrls: ['./member.component.scss']
 })
 export class MemberComponent {
- 
 
-  constructor() {
+   
+
+  tableData : any = [];
+
+
+  constructor(private clientservice: ClientService) { }
+
+  ngOnInit():void{
+    this.getClientData();
   }
 
   showModal = false;
 
-openModal() {
-  this.showModal = true;
-}
+  openModal() {
+    this.showModal = true;
+  }
+  closeModal() {
+    this.showModal = false;
+  }
+  getClientData(){
+    this.clientservice.getClient().subscribe({
+      next:(res:any) => {
+        this.tableData = res;
+        console.log('Client data:',this.tableData);
+      },
+      error:(err) => {
+        console.error('Error Fetching clients',err);
+      }
+    })
+  }
 
-closeModal() {
-  this.showModal = false;
-}
 
 
- 
+
 }

d41a6898e99e96fda74d974e325759964f466464
Anoop <Anooppopziyan@gmail.com>
2026-02-22 19:53:57 +0530

    created common btn,loader components

---
+++ src/app/components/common-btn/common-btn.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-common-btn',
+  standalone: true,
+  imports: [],
+  templateUrl: './common-btn.component.html',
+  styleUrl: './common-btn.component.scss'
+})
+export class CommonBtnComponent {
+
+}
---
+++ src/app/components/common-loader/common-loader.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-common-loader',
+  standalone: true,
+  imports: [],
+  templateUrl: './common-loader.component.html',
+  styleUrl: './common-loader.component.scss'
+})
+export class CommonLoaderComponent {
+
+}
